2c20c99196685b1e6f1cca5696719c0b68a54577
fathirizqiii <103505061+fathirizqiii@users.noreply.github.com>
2022-06-19 10:05:32 +0700

    Add files via upload

---
+++ Titan.java
@@ -0,0 +1,18 @@
+package praktikum_pemlan_8;
+
+public class Titan extends Character {
+    Titan(){
+        setHP(200);
+        setAttack(45);
+        setDefense(0);
+    }
+
+    @Override
+    public boolean attack() {
+        double counter = Math.random() * 10;
+        if (counter > 0 && counter <= 4.0)
+            return true;
+        else
+            return false;
+    }
+}
